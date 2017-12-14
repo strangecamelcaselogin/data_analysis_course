960a701057521461bdc6e0f19cc398acd33c468c
strangecamelcaselogin <strangecamelcaselogin@gmail.com>
2017-12-14 22:49:49 +0300

    Новая структура проектов про однотипные классификаторы

--- classifiers/common.py
+++ classifiers/common.py
@@ -1,8 +1,5 @@
 from collections import namedtuple
-
-from sklearn import cross_validation, svm
 import pandas as pd
-
 
 # данные, ответы, имена столбцов из данных, имена классов
 Dataset = namedtuple('Dataset', ['data', 'target', 'feature_names', 'target_names'])
@@ -30,18 +27,3 @@ def load_data(path: str, complete_data=False):
     persons = df.iloc[:, 1:].as_matrix()
 
     return Dataset(persons, survived, columns[1:], ['Survived', 'Died'])
-
-
-if __name__ == '__main__':
-    titanic_path = '../data/titanic/'
-
-    complete = True
-    train = load_data(titanic_path + 'train.csv', complete)
-    test = load_data(titanic_path + 'test.csv', complete)
-
-    m = svm.SVC()
-    m.fit(train.data, train.target)
-
-    survival_prediction = m.predict(test.data)
-
-    print('acc = {}%, tested {} total.'.format((survival_prediction == test.target).mean(), len(survival_prediction)))
---
+++ classifiers/dt.py
@@ -0,0 +1,46 @@
+import graphviz
+
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+
+from common import load_data
+
+
+def dt_viz(model, feature_names, target_names, name):
+    """
+    визуализация дерева http://scikit-learn.org/stable/modules/tree.html
+    :param model: модель DecisionTreeClassifier
+    :param feature_names: имена признаков
+    :param target_names: имена классов
+    """
+    dot_data = export_graphviz(model,
+                               out_file=None,
+                               feature_names=feature_names,
+                               class_names=target_names,
+                               filled=True,
+                               rounded=True,
+                               special_characters=True)
+    graph = graphviz.Source(dot_data)
+    graph.render(name)
+
+
+if __name__ == '__main__':
+    """
+    https://www.kaggle.com/c/titanic/data
+    """
+
+    titanic_path = '../data/titanic/'
+
+    complete = True
+    train = load_data(titanic_path + 'train.csv', complete)
+    test = load_data(titanic_path + 'test.csv', complete)
+
+    dt = DecisionTreeClassifier(min_samples_split=10, random_state=0)
+
+    dt.fit(train.data, train.target)
+
+    survival_prediction = dt.predict(test.data)
+
+    print('acc = {}%, tested {} total.'.format((survival_prediction == test.target).mean(), len(survival_prediction)))
+
+    dt_viz(dt, train.feature_names, train.target_names, "titanic.tmp")
+
